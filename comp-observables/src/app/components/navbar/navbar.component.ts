e4d5cca9d2b0ea5228bf482605f09105d8b5ed86
Nohely <nohely.badillo96@gmail.com>
2022-02-28 11:25:04 -0600

    funcionalidad de los observables

--- comp-observables/src/app/components/navbar/navbar.component.ts
+++ comp-observables/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string = 'Navbar!';
+
+  constructor( private dataService: DataService ) { }
 
   ngOnInit() {
+    this.dataService.nombreEvent.subscribe( texto => {
+      this.mensaje = texto;
+      console.log('navbar:', texto)
+    })
   }
 
 }
