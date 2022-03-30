16032863ec8489d766faa12e6d2ade72a71b0d29
Nohely <nohely.badillo96@gmail.com>
2022-03-30 00:36:26 -0600

    se ajustaron algunos docs para prácticas con componentes

--- toh-app/src/app/app.component.ts
+++ toh-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+import { Product } from './components/practice/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,62 +7,5 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hola Mundo! Esta es mi primera app con Angular';
-  name = "Nohely";
-  advance = 26;
-  btnDisabled = true;
-
-  register = {
-    name: '',
-    email: '',
-    password: ''
-  }
-
-  box = {
-    width: 100,
-    height: 100,
-    background: 'purple'
-  }
-
-  products: Product[] = [
-    {
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ]
-
-  helloWorld() {
-    console.log("Hola mundito!")
-  }
-
-  onRegister() {
-    console.log(this.register);
-  }
 
 }
